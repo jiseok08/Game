a7c90d8612bd643f2bba14ebdafad7df31a446c0
jiseok08 <heoji0912@gmail.com>
2025-04-16 19:44:43 +0900

    (2025.4.16) 프리팹 시스템

---
+++ Assets/Class5th (Prefab System)/Scripts/Abllity.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Abllity : MonoBehaviour
+{
+    [SerializeField] int strength = 15;
+
+    void Start()
+    {
+        Debug.Log("strengthÀÇ °ª : " + strength);
+    }
+}
