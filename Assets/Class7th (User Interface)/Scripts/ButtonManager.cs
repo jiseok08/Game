ea814261b75c9283c2ccda98f4b422fc5956e679
jiseok08 <heoji0912@gmail.com>
2025-04-21 12:52:51 +0900

    (2025.4.18) 유저 인터페이스

---
+++ Assets/Class7th (User Interface)/Scripts/ButtonManager.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Script : MonoBehaviour
+{
+    public void Execute()
+    {
+        Debug.Log("Execute");
+    }
+
+    public void Shop()
+    {
+        Debug.Log("Shop");
+    }
+
+    public void Quit()
+    {
+        Debug.Log("Ship");
+    }
+}
