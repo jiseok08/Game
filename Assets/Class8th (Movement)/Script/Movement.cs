14e88515be70f3b439be0de8e9e68a9e2f9fc38e
jiseok08 <heoji0912@gmail.com>
2025-04-24 10:37:42 +0900

    (2025.4.23) 충돌 시스템

--- Assets/Class8th (Movement)/Script/Movement.cs
+++ Assets/Class8th (Movement)/Script/Movement.cs
@@ -4,18 +4,18 @@ using UnityEngine;
 
 public class Movement : MonoBehaviour
 {
-    void Start()
-    {
-        transform.position = new Vector3(1,1,1);
-        Debug.Log(transform.position);
-    }
+    [SerializeField] Vector3 direction;
+    [SerializeField] float speed = 1005.0f;
 
     void Update()
     {
-        if(Input.GetKeyDown(KeyCode.W))
-        {
-            transform.position.y++;
-            Debug.Log(transform.position);
-        }
+        direction.x = Input.GetAxisRaw("Horizontal");
+        direction.z = Input.GetAxisRaw("Vertical");
+
+        direction.Normalize();
+
+        // Time.deltaTime이란?
+        // 마지막 프레임이 완료하는데 경과한 시간을 초 단위로 변환하는 시가입니다.
+        transform.position = transform.position + direction * speed *Time.deltaTime;
     }
 }
