ad6ae4954e9051593d5bcd862cd4ee7c06bffdda
jiseok08 <heoji0912@gmail.com>
2025-05-07 19:45:21 +0900

    (2025.5.7) 파티클 시스템

--- Assets/Class12th (Ray Cast)/Scripts/Mouse.cs
+++ Assets/Class12th (Ray Cast)/Scripts/Mouse.cs
@@ -6,7 +6,9 @@ using UnityEngine;
 public class Mouse : MonoBehaviour
 {
     [SerializeField] Ray ray;
+    [SerializeField] LayerMask layerMask;
     [SerializeField] RaycastHit raycastHit;
+    
 
 void Update()
     {
@@ -14,12 +16,21 @@ void Update()
         {
             ray = Camera.main.ScreenPointToRay(Input.mousePosition);
 
-            Debug.Log(ray.origin);
+            if (Physics.Raycast(ray, out raycastHit, Mathf.Infinity, layerMask)) //마우스에 정보를 주는 오브젝트에 닿았다면 out으로 정보 출력
+            {
+                GameObject prefab = raycastHit.collider.gameObject;
+
+                if (prefab.transform.GetChild(0).gameObject.activeSelf)
+                {
+                    prefab.transform.GetChild(0).gameObject.SetActive(false);
+                }
+                else
+                {
+                    prefab.transform.GetChild(0).gameObject.SetActive(true);
+                }
+            }
         }
 
-        if (Physics.Raycast(ray, out raycastHit, Mathf.Infinity)) //마우스에 정보를 주는 오브젝트에 닿았다면 out으로 정보 출력
-        {
-            Debug.Log(raycastHit.collider.GameObject());
-        }
+
     }
 }
